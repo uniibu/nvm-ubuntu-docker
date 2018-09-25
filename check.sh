16533e04006cbb7fe8c02b89b953d14c5c22c61d
uni <unibtc@gmail.com>
2018-09-25 10:47:22 +0800

    use python3

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@
 
 SALT=$(sudo getent shadow ubuntu | cut -d$ -f3)
 KEY=$(sudo getent shadow ubuntu | cut -d: -f2)
-HASH=$(python3 -c 'import crypt; print crypt.crypt("ubuntu", "$6$'${salt}'")')
+HASH=$(python3 -c 'import crypt; print(crypt.crypt("ubuntu", "$6$'$SALT'"))')
 
 if [[ $HASH == $KEY ]]; then
   echo "Change your password"
