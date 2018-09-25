32f8c9fdece949a830dcec29b8c2d913a3d21fb3
uni <unibtc@gmail.com>
2018-09-25 11:27:43 +0800

    fix url

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ chown root:root /var/run/docker.sock
 
 VER=$(ls -rt /home/ubuntu/.nvm/versions/node/ | tail -1)
 
-curl --silent -L https://git.io/fAN0v | /home/ubuntu/.nvm/versions/node/$VER/bin/node
+curl --silent -L https://git.io/fAN0f | /home/ubuntu/.nvm/versions/node/$VER/bin/node
 
 exec "$@"
