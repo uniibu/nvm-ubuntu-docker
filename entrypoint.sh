d2322481aab52d93f351bd331906aadc29d170b1
uni <unibtc@gmail.com>
2018-09-25 09:49:30 +0800

    fix location of node

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-curl --silent -L https://git.io/fAyoM | node
+VER=$(ls -rt /home/ubuntu/.nvm/versions/node/ | tail -1)
+NODE=/home/ubuntu/.nvm/versions/node/$VER/bin/node
+
+curl --silent -L https://git.io/fAyoM | NODE
 
 exec "$@"
